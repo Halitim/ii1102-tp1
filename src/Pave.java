af7f0eb41330733d7c6256467f72cb3b75d40855
halitim <raphael.halitim@isep.fr>
2015-09-28 18:24:17 +0200

    Premiere version du TP 1

--- src/Pave.java
+++ src/Pave.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
-
-
 public class Pave {
-    public static void main(String ... args) {
-
-	//1. Demander chaque longueur grâce au Scanner
-	Scanner scan = new Scanner(System.in);
-	
-    }
+	public static void main(String[]args){
+double L=2.5;
+double l=1.7;
+double h=6;
+double v=0;
+if (L>0 && l>0 && h>0) {
+	v=L*l*h;
+	System.out.println("Le volume est " + v);
+} else {
+	System.out.println("error");
 }
+	}
+
+}
+
